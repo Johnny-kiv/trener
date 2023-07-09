6bb8c9c1e3904d47713da67810f649d9df2d2c75
Johnny-kiv <johnny.kiv@yandex.ru>
2023-07-09 09:55:49 +0300

    mll

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import random
 from aiogram import Bot,Dispatcher,executor,types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 bot = Bot("6005481092:AAFT4kGkrsx4X4bNLfF8XoO4Ou7pIcH44E8")
-dp = Dispatcher(bot=bot)
+dp = Dispatcher(bot=bot,storage=MemoryStorage())
 
 def umn(ras):
     n1=random.randint(int("1"+"0"*(ras-1)),int("10"+"0"*(ras-1)))
@@ -23,7 +24,7 @@ async def start(mes:types.Message,state:FSMContext):
 Введите 2 для сложения - вычитания.
 Введите 3 для нереально трудного режима все в перемешку""")
     await state.set_state(number.wait_type.state)
-@dp.message_handler()
+@dp.message_handler(state=number.wait_type)
 async def type(mes: types.Message,state: FSMContext):
     if mes.text=="":
         await mes.answer("Поле пустое, попробуйте еще раз:")
@@ -31,21 +32,22 @@ async def type(mes: types.Message,state: FSMContext):
     await state.update_data(type=int(mes.text))
     await mes.answer("Сколько разрядов вам для первого раза?")
     await state.set_state(number.wait_count.state)
-@dp.message_handler()
+@dp.message_handler(state=number.wait_count)
 async def count(mes: types.Message,state: FSMContext):
     if mes.text=="":
         await mes.answer("Поле пустое, попробуйте еще раз:")
         return
     await state.update_data(count=int(mes.text))
     global inp
-    user_type = state.get_data()["type"]
-    if user_type == 1:
+    #user_type = state.get_data()["type"]
+    """if user_type == 1:
         inp = umn(mes.text)
     else:
-        inp= "Sorry"
+        inp= "Sorry"""
+    inp = umn(int(mes.text))
     await mes.answer(inp[0])
     await state.set_state(number.wait_answer.state)
-@dp.message_handler()
+@dp.message_handler(state=number.wait_answer)
 async def answer(mes: types.Message,state: FSMContext):
     if mes.text=="":
         await mes.answer("Поле пустое, попробуйте еще раз:")
